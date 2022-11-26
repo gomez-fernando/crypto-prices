814b177cfd0ae1f03f30efd1eb7c14a60b940b7e
gomez-fernando <ferdgomez@hotmail.es>
2022-11-26 12:38:21 +0100

    Style layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+import BackImg from './assets/img/backg-img.png'
 
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media(min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 2rem;
+  }
+`
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+  text-align: center;
+  font-weight: 700;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+  margin: 80px 0 50px 0;
+  font-size: 34px;
+`
 
 function App() {
 
   return (
-    <h1>App c</h1>
+    <Container>
+      <Image src={BackImg} alt='logos de criptomonedas' />
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+      </div>
+    </Container>
   )
 }
 
